7b18a0ce9fd75dd36402e7347f9209af94062c10
iAmWayne <iraolawayne@gmail.com>
2022-12-18 11:06:35 +0800

    added login form

--- src/js/styles/main.js
+++ src/js/styles/main.js
@@ -1,9 +1,10 @@
 const githubBtn = document.querySelector(".github-btn");
 const hamburgerBtn = document.querySelector(".hamburger");
 const backdrop = document.querySelector(".backdrop");
+const sideLinks = document.querySelectorAll(".side-links");
 
 githubBtn.addEventListener("mouseover", (event) =>
-	githubBtn.firstElementChild.setAttribute("fill", "#00ADB5")
+	githubBtn.firstElementChild.setAttribute("fill", "#F49D1A")
 );
 githubBtn.addEventListener("mouseout", (event) =>
 	githubBtn.firstElementChild.setAttribute("fill", "none")
@@ -13,3 +14,14 @@ hamburgerBtn.addEventListener("click", (event) => {
 	backdrop.classList.toggle("hidden");
 	hamburgerBtn.classList.toggle("active");
 });
+
+sideLinks.forEach((el) => [
+	el.addEventListener("mouseover", (e) => {
+		e.currentTarget.firstElementChild.setAttribute("fill", "#F49D1A");
+	}),
+]);
+sideLinks.forEach((el) => [
+	el.addEventListener("mouseleave", (e) => {
+		e.currentTarget.firstElementChild.setAttribute("fill", "#878C8D");
+	}),
+]);
